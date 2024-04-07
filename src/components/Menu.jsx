b87f418b970af4ccaaf994dd08b71e30e363725d
Nati Forish <forish.nati@gmail.com>
2024-04-07 17:07:03 +0300

    name change

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -173,7 +173,7 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: 'none', sm: 'block' } }}
           >
-            MUI
+            Wavy
           </Typography>
           <Search>
             <SearchIconWrapper>
