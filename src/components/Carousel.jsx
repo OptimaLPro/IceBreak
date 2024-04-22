5ca6ab2ef3ed6728d66f0136ce4daaf157273bb4
Nati Forish <forish.nati@gmail.com>
2024-04-22 15:46:09 +0300

    Change Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,47 +1,24 @@
-import React from "react"
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
-import "./styles.css"
+import React from 'react'
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react' // import from 'keen-slider/react.es' for to get an ES module
 
-const carousel = (slider) => {
-  const z = 300
-  function rotate() {
-    const deg = 360 * slider.track.details.progress
-    slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`
-  }
-  slider.on("created", () => {
-    const deg = 360 / slider.slides.length
-    slider.slides.forEach((element, idx) => {
-      element.style.transform = `rotateY(${deg * idx}deg) translateZ(${z}px)`
-    })
-    rotate()
-  })
-  slider.on("detailsChanged", rotate)
-}
-
-export default function App() {
-  const [sliderRef] = useKeenSlider(
+export default () => {
+  const [sliderRef, instanceRef] = useKeenSlider(
     {
-      loop: true,
-      selector: ".carousel__cell",
-      renderMode: "custom",
-      mode: "free-snap",
+      slideChanged() {
+        console.log('slide changed')
+      },
     },
-    [carousel]
+    [
+      // add plugins here
+    ]
   )
 
   return (
-    <div className="wrapper">
-      <div className="scene">
-        <div className="carousel keen-slider" ref={sliderRef}>
-          <div className="carousel__cell number-slide1 ">1</div>
-          <div className="carousel__cell number-slide2">2</div>
-          <div className="carousel__cell number-slide3">3</div>
-          <div className="carousel__cell number-slide4">4</div>
-          <div className="carousel__cell number-slide5">5</div>
-          <div className="carousel__cell number-slide6">6</div>
-        </div>
-      </div>
+    <div ref={sliderRef} className="keen-slider">
+      <div className="keen-slider__slide">1</div>
+      <div className="keen-slider__slide">2</div>
+      <div className="keen-slider__slide">3</div>
     </div>
   )
 }
