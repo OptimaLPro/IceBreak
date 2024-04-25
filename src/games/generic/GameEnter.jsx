6fb712e83b5031d6111fc7ad55065fecbd891334
Nati Forish <forish.nati@gmail.com>
2024-04-25 16:40:38 +0300

    Game Enter Page Created

--- src/games/generic/GameEnter.jsx
+++ src/games/generic/GameEnter.jsx
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
 import AnimatedPage from "../../AnimatedPage";
-
+import Paper from '@mui/material/Paper';
+import { TextField } from "@mui/material";
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/themes/theme-blue.css"; // Import the button styles
 
 const GameEnter = () => {
     return (<>
-        <AnimatedPage >
-            <div>Hello world</div>
-            <Link to="/">Back</Link>
+        <AnimatedPage>
+            <div className="content">
+                <h1 className="page-title">Game Enter</h1>
+
+                <Paper elevation={8} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '30px', borderRadius: '15px' }}>
+                    <div style={{ marginBottom: '20px' }}>
+                        <TextField id="outlined-basic" label="Enter Room PIN" variant="outlined" />
+                    </div>
+                    <div>
+                        <AwesomeButton type="primary" style={{ width: '100%' }}>Enter Game!</AwesomeButton>
+                    </div>
+                </Paper>
+            </div>
         </AnimatedPage>
     </>);
 }
