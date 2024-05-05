f003bf5a9c666a49605f1768bca7f94c4247842a
Nati Forish <forish.nati@gmail.com>
2024-05-05 17:42:59 +0300

    Game are not from GameCard component. data gained from gameData.js. Now responsive for PC and Mobile.

--- src/library/mainPage/mainPageComponents/GameCard.jsx
+++ src/library/mainPage/mainPageComponents/GameCard.jsx
@@ -1,14 +1,63 @@
-import Flicking from "@egjs/react-flicking";
+import React, { useState } from 'react';
+// import Trivia from '../../../assets/images/Trivia.png';
+// import Spy from '../../assets/images/Spy.png';
+// import Answer from '../../assets/images/Answer.png';
+// import WouldYouRather from '../../assets/images/WouldYouRather.png';
+import { Button } from '@mui/material';
+import ReactCardFlip from "react-card-flip";
+import { Link } from 'react-router-dom';
+import '../mainPage.css';
 
-export default () => (
-  <Flicking
-    align="prev"
-    circular={true}
-    onMoveEnd={e => {
-      console.log(e);
-    }}>
-    <div className="panel">1</div>
-    <div className="panel">2</div>
-    <div className="panel">3</div>
-  </Flicking>
-)
+const gameCard = ({ game }) => {
+
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      // If the click target is a button, don't flip the card
+      console.log('Button clicked');
+      return;
+    }
+    setIsFlipped(!isFlipped);
+  }
+  return (
+    <>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+
+        <div className={`game-card ${game.class}`} onClick={handleClick}>
+          <div className="game-card-content">
+            <div className="game-card-title">
+              {game.title}
+            </div>
+            <div className="game-card-img" style={{ overflow: 'visible' }}>
+              <img
+                src={game.image}
+                alt={game.image}
+                className={game.name}
+                loading="lazy" />
+            </div>
+            <div className="game-card-text">
+              {game.trailer}
+            </div>
+          </div>
+        </div>
+
+        <div className={`game-card ${game.class}`} onClick={handleClick}>
+          <div className="game-card-content">
+            <div className="game-card-header">
+              <div className="game-card-title">
+                {game.title}
+              </div>
+              <Button variant="contained" color="ochre">Play!</Button>
+            </div>
+            <div className="game-card-explanation">
+              {game.description}
+            </div>
+          </div>
+        </div>
+      </ReactCardFlip>
+    </>
+  );
+}
+
+export default gameCard;
