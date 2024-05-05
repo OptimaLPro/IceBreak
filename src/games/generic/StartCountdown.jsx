02c196f6a4d6a368e16184f18d43addc1a08394b
Sahar <96saharyosef@gmail.com>
2024-05-05 22:52:49 +0300

    a

--- src/games/generic/StartCountdown.jsx
+++ src/games/generic/StartCountdown.jsx
@@ -1,24 +1,24 @@
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
-import AnimatedPage from '../../AnimatedPage';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import AnimatedPage from "../../AnimatedPage";
 
 const StartCountdown = () => {
-    return (
-        <>
-            <AnimatedPage>
-                <div className="content">
-                    <h2>The game is starting!</h2>
-                    <CountdownCircleTimer
-                        isPlaying
-                        duration={7}
-                        colors={['#508D69', '#E8FD96', '#EDA489', '#D15555']}
-                        colorsTime={[7, 5, 2, 0]}
-                    >
-                        {({ remainingTime }) => remainingTime}
-                    </CountdownCircleTimer>
-                </div>
-            </AnimatedPage>
-        </>
-    );
-}
+  return (
+    <>
+      <AnimatedPage>
+        <div className="content">
+          <h2>The game is starting!</h2>
+          <CountdownCircleTimer
+            isPlaying
+            duration={7}
+            colors={["#508D69", "#E8FD96", "#EDA489", "#D15555"]}
+            colorsTime={[7, 5, 2, 0]}
+          >
+            {({ remainingTime }) => remainingTime}
+          </CountdownCircleTimer>
+        </div>
+      </AnimatedPage>
+    </>
+  );
+};
 
 export default StartCountdown;
