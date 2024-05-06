586ef9b58ea35692797aaa509a4e904e6ac4f302
Sahar <96saharyosef@gmail.com>
2024-05-06 16:11:29 +0300

    loginPages

---
+++ src/library/logInPages/SignIn.jsx
@@ -0,0 +1,136 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import "../../assets/css/loginPages.css";
+
+function validateEmail(email) {
+  // Regular expression for basic email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  // Check if password is at least 8 characters long
+  return password.length >= 8;
+}
+
+function SignIn() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [emailError, setEmailError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Reset error states
+    setEmailError(false);
+    setPasswordError(false);
+
+    // Validation
+    if (!validateEmail(email)) {
+      setEmailError(true);
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      setPasswordError(true);
+      return;
+    }
+
+    // If validation passes, continue with form submission
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={createTheme()}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              error={emailError}
+              onChange={(e) => setEmail(e.target.value)}
+              helperText={emailError ? "Please enter a valid email" : ""}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              error={passwordError}
+              onChange={(e) => setPassword(e.target.value)}
+              helperText={passwordError ? "invalid Password" : ""}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default SignIn;
---
+++ src/library/logInPages/SignUp.jsx
@@ -0,0 +1,162 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import React, { useState } from "react";
+import "../../assets/css/loginPages.css";
+
+const defaultTheme = createTheme();
+
+export default function SignUp() {
+  const [validations, setValidations] = useState({
+    firstName: true,
+    lastName: true,
+    email: true,
+    password: true,
+    confirmPassword: true,
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const formData = Object.fromEntries(data.entries());
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const newValidations = Object.fromEntries(
+      Object.entries(formData).map(([name, value]) => {
+        switch (name) {
+          case "email":
+            return [name, emailRegex.test(value)];
+          case "password":
+            return [name, value.length >= 8];
+          case "confirmPassword":
+            return [name, value === formData.password];
+          default:
+            return [name, !!value];
+        }
+      })
+    );
+
+    setValidations(newValidations);
+
+    if (Object.values(newValidations).every((isValid) => isValid)) {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              {[
+                {
+                  name: "firstName",
+                  label: "First Name",
+                  autoComplete: "given-name",
+                },
+                {
+                  name: "lastName",
+                  label: "Last Name",
+                  autoComplete: "family-name",
+                },
+                {
+                  name: "email",
+                  label: "Email Address",
+                  autoComplete: "email",
+                },
+                {
+                  name: "password",
+                  label: "Password",
+                  type: "password",
+                  autoComplete: "new-password",
+                },
+                {
+                  name: "confirmPassword",
+                  label: "Confirm Password",
+                  type: "password",
+                  autoComplete: "confirm-password",
+                },
+              ].map((field) => (
+                <Grid item xs={12} key={field.name}>
+                  <TextField
+                    required
+                    fullWidth
+                    name={field.name}
+                    label={
+                      validations[field.name]
+                        ? field.label
+                        : "Invalid Information"
+                    }
+                    type={field.type || "text"}
+                    id={field.name}
+                    autoComplete={field.autoComplete}
+                    error={!validations[field.name]}
+                    onChange={(e) =>
+                      setValidations({
+                        ...validations,
+                        [field.name]: !!e.target.value,
+                      })
+                    }
+                    InputProps={{ style: { backgroundColor: "inherit" } }}
+                    InputLabelProps={{ style: { backgroundColor: "inherit" } }}
+                  />
+                </Grid>
+              ))}
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="./SignIn" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
