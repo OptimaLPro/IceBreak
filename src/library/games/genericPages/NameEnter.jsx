b8feb7933b1dbc324cf98a430a5ce55f69c6e505
Nati Forish <forish.nati@gmail.com>
2024-05-08 19:24:06 +0300

    Fix avatar, now using DiceBear

--- src/library/games/genericPages/NameEnter.jsx
+++ src/library/games/genericPages/NameEnter.jsx
@@ -4,14 +4,22 @@ import AnimatedPage from "../../../theme/AnimatedPage";
 import Paper from '@mui/material/Paper';
 import { TextField } from "@mui/material";
 import { AwesomeButton } from "react-awesome-button";
-import "react-awesome-button/dist/themes/theme-blue.css"; // Import the button styles
-import Avatar, { genConfig } from 'react-nice-avatar'
+import "react-awesome-button/dist/themes/theme-blue.css";
+import { createAvatar } from '@dicebear/core';
+import { micah } from '@dicebear/collection';
 
 const NameEnter = () => {
-    const [config, setConfig] = useState(genConfig());
+    const [avatarSeed, setAvatarSeed] = useState(Date.now());
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    const avatar = createAvatar(micah, {
+        seed: avatarSeed.toString(),
+        size: 128,
+        backgroundColor: [randomColor]
+    }).toDataUriSync();
 
     const handleRandomizeConfig = () => {
-        setConfig(genConfig());
+        setAvatarSeed(Date.now());
     };
 
     return (
@@ -21,14 +29,14 @@ const NameEnter = () => {
                 <Paper elevation={8} sx={{
                     display: 'flex',
                     flexDirection: 'column',
-                    alignItems: 'center', // Center items horizontally
+                    alignItems: 'center',
                     padding: '30px',
                     borderRadius: '15px'
                 }}>
                     <div style={{ marginBottom: '20px' }}>
-                        <Avatar style={{ width: '100px', height: '100px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)' }} {...config} />
+                        <img src={avatar} alt="Avatar" style={{ width: '100px', height: '100px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)', borderRadius: '50px' }} />
                     </div>
-                    <AwesomeButton size="small" type="secondary" style={{ width: '87%', marginBottom: '20px'}} onPress={handleRandomizeConfig}>Random Avatar 🎲</AwesomeButton>
+                    <AwesomeButton size="small" type="secondary" style={{ width: '87%', marginBottom: '20px' }} onPress={handleRandomizeConfig}>Random Avatar 🎲</AwesomeButton>
 
                     <div style={{ width: '100%', marginBottom: '20px', marginTop: '10px' }}>
                         <TextField id="outlined-basic" label="Enter Your Name" variant="outlined" fullWidth />
