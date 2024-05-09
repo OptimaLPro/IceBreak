28ddbd5213131682912b093b26635dac4bb31f4b
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2024-05-09 18:10:21 +0300

    create dummy data

---
+++ src/library/userProfile/dummyData.js
@@ -0,0 +1,28 @@
+
+User ={
+    name: 'John Doe',
+    email: 'lolo@gmail.com',
+    avatar: '/static/images/avatar/5.jpg',
+    favorites: [
+        {
+            primary: 'Brunch this week?',
+            secondary: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
+            person: '/static/images/avatar/5.jpg',
+        },
+        {
+            primary: 'Birthday Gift',
+            secondary: `Do you have a suggestion for a good present for John on his work`
+        }
+    ],
+    recents: [
+        {
+            primary: 'Brunch this week?',
+            secondary: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
+            person: '/static/images/avatar/5.jpg',
+        },
+        {
+            primary: 'Birthday Gift',
+            secondary: `Do you have a suggestion for a good present for John on his work`
+        }
+    ]
+}
