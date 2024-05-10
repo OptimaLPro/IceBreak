513ba6bb844b545fd31da3a5c451621ae4a29cb5
Nati Forish <forish.nati@gmail.com>
2024-05-10 15:16:41 +0300

    Sruvey change style

--- src/library/mainPage/components/Survey.jsx
+++ src/library/mainPage/components/Survey.jsx
@@ -3,34 +3,21 @@ import './Survey.css';
 import Slider from "@mui/material/Slider";
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-import { Paper } from '@mui/material';
-
-
+import { MenuItem, Paper, Select } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { AwesomeButton } from 'react-awesome-button';
 
 const Survey = () => {
-
-    // State to hold the value of the number of players slider
     const [numPlayers, setNumPlayers] = useState(0);
-
-    // State to hold the value of the game time slider
     const [gameTime, setGameTime] = useState(0);
+    const [selectedAtmosphere, setSelectedAtmosphere] = useState("Short");
 
-    // State to hold the selected game duration option
-    const [selectedAtmosphere, setSelectedAtmosphere] = useState("30 minutes");
-
-    // Event handler to update the number of players slider value
     const handleNumPlayersChange = (event) => {
         setNumPlayers(parseInt(event.target.value));
     };
 
-    // Event handler to update the game time slider value
     const handleGameTimeChange = (event) => {
         setGameTime(parseInt(event.target.value));
-    };
-
-    // Event handler to handle dropdown selection
-    const handleAtmosphereChange = (event) => {
-        setSelectedAtmosphere(event.target.value);
     };
 
     const handleSkip = () => {
@@ -38,32 +25,34 @@ const Survey = () => {
         console.log("Survey skipped");
     };
 
+    const handleChange = (event) => {
+        setSelectedAtmosphere(event.target.value);
+    };
 
     return (
         <>
-            {/*  <div className="survey-wrapper"> */}
             <div className="survey-header">Before we start..</div>
-            {/* <div className="survey-container"> */}
             <Paper elevation={8} sx={{
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
                 padding: '30px',
-                borderRadius: '15px'
+                borderRadius: '15px',
+                width: '70%',
             }}>
                 <div className="numPlayers"></div>
-                <h3>Number of players:</h3>
+                <h3>Number of players: <span className="slider-value">{numPlayers}</span></h3>
                 <Slider
                     value={numPlayers}
                     onChange={handleNumPlayersChange}
                     aria-labelledby="discrete-slider"
                     valueLabelDisplay="auto"
-                    step={1}
+                    step={10}
                     min={0}
                     max={100}
                 />
-                <div className="slider-value">{numPlayers}</div>
-                <h3>Game time:</h3>
+
+                <h3>Game time: <span className="slider-value">{gameTime}</span></h3>
                 <Slider
                     value={gameTime}
                     onChange={handleGameTimeChange}
@@ -73,20 +62,30 @@ const Survey = () => {
                     min={0}
                     max={100}
                 />
-                <div className="slider-value">{gameTime}</div>
-                <h3>Atmosphere:</h3>
-                <select value={selectedAtmosphere} onChange={handleAtmosphereChange} className="dropdown">
-                    <option value="chooos">Please choose</option>
-                    <option value="option1">option1</option>
-                    <option value="option2">option2</option>
-                    <option value="option3">option3</option>
-                </select>
-                {/* </div> */}
-                {/* <button className="skip-button" onClick={handleSkip}>Skip</button> */}
-                <Button variant="contained" endIcon={<SendIcon />}>
-                    Send
-                </Button>
-                {/* </div> */}
+
+                <div className='atmosphere-container'>
+                    <h3>Atmosphere:</h3>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={selectedAtmosphere}
+                        label="Age"
+                        onChange={handleChange}
+                        sx={{ width: '40%' }}
+                    >
+                        <MenuItem value={"Short"}>Short</MenuItem>
+                        <MenuItem value={"Funny"}>Funny</MenuItem>
+                        <MenuItem value={"Drinking"}>Drinking</MenuItem>
+                    </Select>
+                </div>
+                <div className='buttons-container'>
+                    <Link to="/">
+                        <AwesomeButton size="xl" type="primary" className="survey-button">Let's get it started 🔥</AwesomeButton>
+                    </Link>
+                    <Link to="/">
+                        <AwesomeButton size="small" type="primary" className="survey-button aws-btn--grey">Skip</AwesomeButton>
+                    </Link>
+                </div>
             </Paper>
         </>
     );
